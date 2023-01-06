90bcf21d602e5ea39699d03145b6f601158e902e
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-01-06 13:23:41 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,46 @@
+import { serve } from 'https://deno.land/std@0.156.0/http/server.ts';
+
+const clients = new Map<number, WebSocket>();
+const usernames = new Map<number, string>();
+let clientId = 0;
+
+const sendMessage = (message: string) => {
+  clients.forEach((client) => {
+    client.send(message);
+  });
+};
+
+const wsHandler = (ws: WebSocket) => {
+  const id = ++clientId;
+  clients.set(id, ws);
+  ws.onopen = () => {};
+  ws.onmessage = (e) => {
+    const payload = JSON.parse(e.data);
+
+    if (payload.type === 'join') {
+      usernames.set(id, payload.data.author);
+    }
+    sendMessage(e.data);
+  };
+  ws.onclose = () => {
+    clients.delete(id);
+    sendMessage(
+      JSON.stringify({
+        type: 'left',
+        data: {
+          author: usernames.get(id),
+          ts: Date.now(),
+        },
+      })
+    );
+    usernames.delete(id);
+  };
+};
+
+serve((req) => {
+  const { response, socket } = Deno.upgradeWebSocket(req);
+
+  wsHandler(socket);
+
+  return response;
+});
